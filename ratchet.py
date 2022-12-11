f11a6a655e4db35975d1e3c66d886e8a52f41a2e
FreddyJS <sebashgt@gmail.com>
2022-12-11 21:41:18 +0100

    Done

--- ratchet.py
+++ ratchet.py
@@ -1,3 +1,5 @@
+import dh_parameters
+
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -9,9 +11,40 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.hmac import HMAC
 
 
-TAG_LENGTH = 32
+DH_PARAMETERS = dh.DHParameterNumbers(dh_parameters.P, dh_parameters.G).parameters(default_backend())
 DERIVED_KEY_LENGTH = 80
 INFO = b'handshake'
+TAG_LENGTH = 32
+
+
+def generate_dh():
+    return DH_PARAMETERS.generate_private_key()
+
+
+def dh_output(private_key: dh.DHPrivateKey, public_key: dh.DHPublicKey) -> bytes:
+    return private_key.exchange(public_key)
+
+
+def kdf_rk(rk: bytes, dh_output: bytes) -> bytes:
+    result = HKDF(
+        algorithm=SHA256(),
+        length=32,
+        salt=b'\x00' * 32,
+        info=INFO,
+    ).derive(rk + dh_output)
+
+    return result[:16], result[16:]
+
+
+def kdf_ck(ck: bytes) -> bytes:
+    result = HKDF(
+        algorithm=SHA256(),
+        length=32,
+        salt=b'\x00' * 32,
+        info=INFO,
+    ).derive(ck)
+
+    return result[:16], result[16:]
 
 
 def derive(key, info):
@@ -76,13 +109,13 @@ def decrypt(ciphertext: bytes, key: bytes, associated_data: bytes) -> bytes:
     decryption_key, authentication_key, iv = derive(key, INFO)
 
     # Split the authentication tag from the ciphertext
-    tag = ciphertext[-32:]
-    ciphertext = ciphertext[:-32]
+    tag = ciphertext[-TAG_LENGTH:]
+    ciphertext = ciphertext[:-TAG_LENGTH]
 
     # Verify the authentication tag
     calculated_tag = hmac(authentication_key, associated_data + ciphertext)
     if tag != calculated_tag:
-        raise Exception('Invalid tag')
+        raise Exception(f'Invalid tag! Expected {tag.hex()}, got {calculated_tag.hex()}')
 
     # Decrypt the ciphertext using AES-CBC with PKCS7 padding
     return aes_cbc_decrypt(decryption_key, iv, ciphertext)
---
+++ server.py
@@ -0,0 +1,78 @@
+import time
+import ratchet
+import dh_parameters
+
+import paho.mqtt.client as mqtt
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.backends import default_backend
+
+ROOT_KEY = b'>\xe0+\xd7\x97]\x1e4\xb1\xca\xc6\xa3\x07\xba]\x1c'
+CHAIN_KEY = None
+
+KEY_PAIR = ratchet.generate_dh()
+PEER_KEY = None
+
+MQTT_BROKER = 'broker.hivemq.com'
+MQTT_PORT = 1883
+
+def print_green(text: str):
+    # Get the current time in [HH:MM:SS] format
+    time_str = time.strftime('[%H:%M:%S]', time.localtime())
+
+    # Print the time in white, followed by the text in green
+    print(f'\033[97m{time_str} \033[92m{text}\033[0m')
+
+def mqtt_on_message(client: mqtt.Client, userdata: dict, msg: mqtt.MQTTMessage):
+    global ROOT_KEY, CHAIN_KEY, PEER_KEY
+
+    public_key = msg.payload.split(b'\n-----END PUBLIC KEY-----')[0] + b'\n-----END PUBLIC KEY-----'
+    if PEER_KEY != public_key:
+        # New peer key
+        dhpublic_key: dh.DHPublicKey = serialization.load_pem_public_key(public_key, backend=default_backend())
+        dh_output = ratchet.dh_output(KEY_PAIR, dhpublic_key)
+
+        ROOT_KEY, CHAIN_KEY = ratchet.kdf_rk(dh_parameters.ROOT_KEY, dh_output)
+        PEER_KEY = public_key
+
+    if len(msg.payload) == len(public_key):
+        mqtt_client.publish('tor.in', KEY_PAIR.public_key().public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo).strip())
+        return
+
+    CHAIN_KEY, message_key = ratchet.kdf_ck(CHAIN_KEY)
+    cypertext = msg.payload.split(b'\n-----END PUBLIC KEY-----')[1]
+    try:
+        plaintext = ratchet.decrypt(cypertext, message_key, b'AES-256-CBC')
+        print_green(plaintext.decode())
+    except Exception as e:
+        print('Failed to decrypt message')
+        print('ERROR: ' + str(e) + '\n')
+
+
+def mqtt_on_connect(client: mqtt.Client, userdata: dict, flags, rc):
+    client.subscribe('tor.out')
+
+
+mqtt_client = mqtt.Client('tor_server')
+mqtt_client.on_message = mqtt_on_message
+mqtt_client.on_connect = mqtt_on_connect
+mqtt_client.connect(host=MQTT_BROKER, port=MQTT_PORT)
+mqtt_client.loop_start()
+
+while not mqtt_client.is_connected():
+    time.sleep(0.1)
+
+print()
+print_green('Welcome to the Tor chat server! Type "exit" to quit.')
+while True:
+    message = input('')
+    if message == 'exit':
+        break
+
+    CHAIN_KEY, message_key = ratchet.kdf_ck(CHAIN_KEY)
+    cypertext = ratchet.encrypt(message.encode(), message_key, b'AES-256-CBC')
+    header = KEY_PAIR.public_key().public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo).strip()
+
+    mqtt_message = header + cypertext
+    mqtt_client.publish('tor.in', mqtt_message)
+    time.sleep(0.25)
